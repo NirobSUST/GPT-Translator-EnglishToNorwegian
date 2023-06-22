c55cd3be953425c11320af13adb5e84e489d9fd8
NirobSUST <nirobsyclonite@gmail.com>
2023-06-22 17:18:36 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@ from pathlib import Path
 def get_config():
     return {
         "batch_size": 8,
-        "num_epochs": 20,
+        "num_epochs": 100,
         "lr": 10**-4,
         "seq_len": 350,
         "d_model": 512,
-        "lang_src": "en",
-        "lang_tgt": "no",
+        "lang_src": "no",
+        "lang_tgt": "en",
         "model_folder": "weights",
         "model_basename": "tmodel_",
         "preload": None,
